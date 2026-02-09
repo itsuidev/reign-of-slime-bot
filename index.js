391833408e9dc3f04957c45c1097317af7ccc8ff
itsuidev <itsuidev@gmail.com>
2026-02-09 01:54:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import "dotenv/config";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.once("clientReady", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  if (message.content === "!ping") message.reply("🏓 Pong!");
+});
+
+client.login(process.env.DISCORD_TOKEN);
