1e1f511b2beb57f0d2e3f6aca1b8651e1fc6bd71
itsuidev <itsuidev@gmail.com>
2026-02-09 02:18:35 +0100

    Added slash commands and deploy script

---
+++ commands/botinfo.js
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+  .setName("botinfo")
+  .setDescription("Shows bot info.");
+
+export async function execute(interaction) {
+  const embed = new EmbedBuilder()
+    .setTitle("Great Slime of Wisdom")
+    .setDescription("Official Reign of Slime helper bot.")
+    .setColor(0x00ff00)
+    .addFields(
+      { name: "Author", value: "Itsui" },
+      { name: "Version", value: "0.0.1" }
+    )
+    .setFooter({ text: "Great Slime of Wisdom" })
+    .setTimestamp();
+
+  await interaction.reply({ embeds: [embed] });
+}
